15ab760cb4f2f2abd23d7e9cf48f2babe758b89d
nirportal <55716718+nirportal@users.noreply.github.com>
2022-03-15 23:06:59 +0200

    section 6 complete

--- main.py
+++ main.py
@@ -72,20 +72,65 @@ picture 7:
 #word = input("enter a word: ")
 #print('_ '*len(word))
 
-guess_letter = input("Guess a letter: ")
+#guess_letter = input("Guess a letter: ")
 
 
-def is_valid_input(letter_guessed):
+def check_valid_input(letter_guessed, old_letters_guessed):
     """
     this function checks if the letter guessed is valid
     for my game: the letter must be one letter
-    in english only.
-    :param letter_guessed: the letter guested from the user
+    in english only and the user didn't already guess it before.
+    the old_letters_guessed list.
+    :param: letter_guessed: the letter guested from the user
+    :param: old_letters_guessed: a list of the letters
+    already guested
     :type: string
-    :return: returns if the letter is valid (True or False).
+    :type: list
+    :return: returns if the letter is valid
     :rtype: bool
     """
-    if len(letter_guessed) > 1 or not(letter_guessed.isalpha()):
-        return False
-    elif len(letter_guessed) == 1 and letter_guessed.isalpha():
+    return len(letter_guessed) == 1 and letter_guessed.isalpha() \
+           and letter_guessed.lower() not in old_letters_guessed
+
+
+def try_update_letter_guessed(letter_guessed, old_letters_guessed):
+    """
+    the function checks if the letter guessed is valid
+    :param: letter_guessed: the user input
+    :param: old_letters_guessed: the user old letters input
+    :type: string
+    :type: list
+    :return: if True - it adds it to the old letters list, return True
+    if False - prints X and the list sorted by size and lowered case
+    with a -> between each value in the list, returns False
+    :rtype: bool
+    """
+    if check_valid_input(letter_guessed, old_letters_guessed):
+        old_letters_guessed.append(letter_guessed.lower())
         return True
+    else:
+        print("X")
+        sorted(old_letters_guessed)
+        print(organize_list_as_string(old_letters_guessed))
+        return False
+
+
+def organize_list_as_string(old_letters_guessed):
+    """
+    organizes a list to a certain format: a sorted list
+    and a -> between each value:
+    a -> b -> c
+    :param: old_letters_guessed: the old letters the user inputted
+    :type: list
+    :return: the new formatted string
+    :rtype: string
+    """
+    new_string = " -> ".join(sorted(old_letters_guessed))
+    return new_string
+
+
+old_letters = ['a', 'p', 'c', 'f']
+print(try_update_letter_guessed('A', old_letters))
+print(try_update_letter_guessed('s', old_letters))
+print(try_update_letter_guessed('$', old_letters))
+print(try_update_letter_guessed('d', old_letters))
